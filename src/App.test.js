89a8530d8c6e6e694ef676396a9a99ec10ff9358
Andres Miguez <andres.miguez@webbeds.com>
2020-07-02 17:36:46 +0200

    fix App tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders default todo', () => {
+test('renders Header todo', () => {
   const { getByText } = render(<App />);
-  const todoElement = getByText(/Have breakfast/i);
-  expect(todoElement).toBeInTheDocument();
+  const headerElement = getByText(/Todo list/i);
+  expect(headerElement).toBeInTheDocument();
 });
