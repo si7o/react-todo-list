d59ff5efd811b879498a7631582630e379bf8f10
Andres Miguez <andres.miguez@webbeds.com>
2020-07-03 13:49:17 +0200

    use a form for adding ToDo(s)

--- src/components/todos/components/todo-add/TodoAdd.jsx
+++ src/components/todos/components/todo-add/TodoAdd.jsx
@@ -4,32 +4,24 @@ import './TodoAdd.scss'
 export default function TodoAdd({addTodoHandler}) {
     const [todoText, setTodoText] = useState('')
 
-    function handleTodoTextChange(event) {
+    const handleTodoTextChange = (event) => {
         setTodoText(event.target.value)
-    }
+    }    
 
-    function handleKeyDown(e) {
-        if (e.key === 'Enter') {
-            addTodoClick()
-        }
-    } 
-
-    function addTodoClick() {
-        addTodoHandler(todoText)        
+    const submitTodo = (e) => {
+        e.preventDefault()
+        addTodoHandler(todoText)
         setTodoText('')
     }
 
     return (
-        <div className="todo-add">
+        <form className="todo-add" onSubmit={submitTodo}>
             <input 
                 type="text"
                 placeholder="type todo text here" 
                 value={todoText} 
-                onChange={handleTodoTextChange}
-                onKeyPress={handleKeyDown}/>
-            <button onClick={addTodoClick} disabled={todoText.length===0}>
-                Add todo
-            </button>
-        </div>
+                onChange={handleTodoTextChange} />
+            <input type="submit" disabled={todoText.length===0} value="Add todo"/>
+        </form>
     )
 }
