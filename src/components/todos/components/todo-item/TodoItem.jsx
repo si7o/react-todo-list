b5bb19e3b47e4d119fe5729bb43ca782b036b3b9
Andres Miguez <andres.miguez@webbeds.com>
2020-07-06 18:30:51 +0200

    style changes

--- src/components/todos/components/todo-item/TodoItem.jsx
+++ src/components/todos/components/todo-item/TodoItem.jsx
@@ -10,13 +10,13 @@ export default function TodoItem({item, index}) {
     return (
         <li className={'todo-item ' + (item.done ? 'completed':'')}>
             <button className="complete" alt="Mark as done" title="Mark as done" onMouseUp={() => todosDispatch({type:TOGGLE_TODO_STATUS, todoText: item.text})}>
-                &#x2713;
+                &#x2714;
             </button>
             <span className="text">
                 {item.text}
             </span>
             <button className="remove" alt="Remove" title="Remove" onClick={() => todosDispatch({type:REMOVE_TODO, todoText: item.text})}>
-                &#x1f5d1;
+                &#x2716;
             </button>
         </li>
     )
